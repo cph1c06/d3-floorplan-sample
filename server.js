a2a0ab2870ad7ee5f0381a954d9f3de1dc11b30c
cph1c06 <hkcph@msn.com>
2019-10-25 12:01:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
+var webshot = require('webshot');
+
 var app = express();
 var port = process.env.PORT || 8080;
+var options = {
+	renderDelay: '6000'
+}
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use('/public',express.static(__dirname + '/public'));
 
-app.get('/res', function (req, res) {
-	webshot('http://127.0.0.1:8080', 'pentacode.png', options, function(err) {
+app.get('/', function (req, res) {
+	webshot('http://127.0.0.1:8080/public', 'pentacode.png', options, function(err) {
 		if (!err) {
 			console.log('Screenshot taken!');
 		}
-		res.sendFile('/app/d3-floorplan-sample/pentacode.png');
+		res.sendFile('/app/pentacode.png');
 	});
-	//res.sendFile('/app/d3-floorplan-sample/pentacode.png');
 });
 
 // Serve your app
